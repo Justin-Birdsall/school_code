1fb99570e77e5a2881ee09b4a942c487c1a47167
Justin_Birdsall <JustinLeRoyBirdsall@gmail.com>
2024-01-09 12:04:11 -0500

    trying to fix whatever wasn't working on my laptop

---
+++ winter_2024/alg_eng380/HW1/main.py
@@ -0,0 +1,10 @@
+
+if __name__ == "__main__":
+    num_tests = int(input())
+
+    for test_idx in range(num_tests):
+        line = input().split()
+        S = int(line[0]) # Starting bet
+        k = int(line[1]) # Number of rounds
+
+        # Your code goes here!
